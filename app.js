3ea0a62d6e842c687fdcc7f1c1c0f3b81a0803ba
benefrihw <gjtja7@naver.com>
2024-06-29 21:46:35 +0900

    chore:초기세팅_#2

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+import cors from 'cors';
 import express from "express";
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+
+app.use(cors());
+app.use(express.json());
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
 
 app.listen(port, () => {
   console.log(port, "포트로 연결되었습니다.");
 });
-
-// 포트연결
